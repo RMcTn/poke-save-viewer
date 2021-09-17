b92f2b29754376125e118e487fbb0ca04e823273
Reece <rmctiernan@pm.me>
2021-09-17 12:11:40 +0100

    Add type badges for gen 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
-  resources :gen3_entries
   resources :gen2_entries
   resources :gen1_entries
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
